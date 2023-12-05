08927451079c3e473bf0daedc1cc9a60eac8df26
jeffrysteve <jeffrysteve2005@gmail.com>
2023-12-05 23:36:44 +0000

    Luzi learned o listen and reapt it again

--- engine/command.py
+++ engine/command.py
@@ -1,5 +1,5 @@
 import pyttsx3
-
+import speech_recognition as sr
 
 def speak(text):
     engine = pyttsx3.init('sapi5')
@@ -10,4 +10,30 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
     
-speak("Hello I am Loossi, your Personal assistant")     
+    
+def TakeCommand():
+    
+    r = sr.Recognizer()
+    
+    with sr.Microphone() as source:
+        print('listening.....')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        
+        audio = r.listen(source, 10, 6)
+        
+    try:
+        print("recognizing")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said: {query}")
+        speak(query)
+        
+    except Exception as e:
+        return ""   
+    
+    return query.lower()
+
+#Fucntion call
+    
+test = TakeCommand()    
+   
